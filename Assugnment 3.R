3a5493157bd3a03e192c25e818e2c4eda6f1527e
Ricardo Barnes <118009498+RicardoBarnes@users.noreply.github.com>
2024-08-29 16:15:50 -0400

    Add files via upload

---
+++ Assugnment 3.R
@@ -0,0 +1,16 @@
+# 1. create 1000 random numbers and store them in a variable called variable1
+# 2. calculate sd, mean, median of variable1
+# 3. Save output of sd, mean, and median in R document along with your statements, name document as assignment3.R
+
+variable1<-rnorm(1000)
+
+sd_of_variable1<-sd(variable1)
+mean_of_variable1<-mean(variable1)
+median_of_variable1<-median(variable1)
+
+
+# median_of_variable1= -0.05756059
+# mean of variable1= -0.05892448
+# sd of variable1= 1.018722
+
+
